b59a50efe3ca170447f05ab94f4d8a40e0fda248
MarcAlKareh <ma7748@student.st-chris.net>
2022-08-22 21:23:24 +1000

    Saved tasks in local storage and retrivied items on page opening so tasks wont be lost

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const taskInputEl = document.querySelector('.task-input');
 const addTaskBtn = document.querySelector('.add-task');
 const tasksContainer = document.querySelector('.tasks-container');
 
+let tasks = [];
+
 const handleTaskIcons = function (e) {
   // TODO
   // Check if user clicked on task icons
@@ -11,6 +13,12 @@ const handleTaskIcons = function (e) {
   ) {
     // Get closest task element and delete it
     const taskEl = e.target.closest('.task');
+
+    // Remove task from tasks list and update localStorage
+    const taskIndex = tasks.indexOf(taskEl.textContent.trim());
+    tasks.splice(taskIndex, 1);
+    updateLocalStorage();
+
     tasksContainer.removeChild(taskEl);
   }
 };
@@ -20,42 +28,46 @@ const addTask = function (task) {
   if (!task)
     return alert('Please write the name of a task and then add the task');
 
+  // Add task to tasks array and add it to localStorage
+  tasks.push(task);
+  updateLocalStorage();
+
   // HTML code for task element
   const html = `
-    <div class="task">
-      <p>${task}</p>
-      <div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="completed-icon"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="delete-icon"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-      </div>
-    </div> 
-  `;
+      <div class="task">
+        <p class="task-name">${task}</p>
+        <div class="icon-container">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="completed-icon"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="delete-icon"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </div>
+      </div> 
+    `;
 
   // Add task element to page
   tasksContainer.insertAdjacentHTML('beforeend', html);
@@ -66,6 +78,26 @@ const addTask = function (task) {
   // Listen for click event on task container
   tasksContainer.addEventListener('click', handleTaskIcons);
 };
+
+// Sets tasks in localStorage
+const updateLocalStorage = function () {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+// Retrieves data from localStorage
+const getTasksLocalStorage = function () {
+  const data = JSON.parse(localStorage.getItem('tasks'));
+  // console.log(data);
+
+  if (!data) return;
+
+  // tasks = data;
+  data.forEach(t => {
+    addTask(t);
+  });
+};
+
+getTasksLocalStorage();
 
 // Listen for click event of add task button and if user clicks enter key
 addTaskBtn.addEventListener('click', () => addTask(taskInputEl.value));
